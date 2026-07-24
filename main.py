88bc4fdac8a5c958aa4eaa1fe33dfc7a6ca85228
svyatikpro <sviatoslav362@gmail.com>
2026-07-24 13:32:59 +0200

    frontend added

--- main.py
+++ main.py
@@ -5,8 +5,23 @@ import models
 from routers.listings import router as listings_router
 from routers.auth import router as auth_router
 from routers.bookings import router as bookings_router
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 app.include_router(listings_router)
 app.include_router(auth_router)
